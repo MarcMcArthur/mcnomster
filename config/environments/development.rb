ac10cbae037a319f4f4f7896290656ad9cb99d04
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-03-28 15:52:10 +0000

    Unknown changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Mcnomster::Application.configure do
 
   Rails.application.routes.default_url_options[:host] = 'mcnomster-marc-mcarthur.herokuapp.com'
   #config.action_mailer.default_url_options = { host: 'mcnomster-marc-mcarthur.herokuapp.com' }
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  #config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Mcnomster::Application.configure do
   ActionMailer::Base.delivery_method = :smtp
 
   Rails.application.routes.default_url_options[:host] = 'mcnomster-marc-mcarthur.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'mcnomster-marc-mcarthur.herokuapp.com' }
+  #config.action_mailer.default_url_options = { host: 'mcnomster-marc-mcarthur.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
